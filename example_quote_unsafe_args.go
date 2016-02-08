5535bc09016e0e2d3fa211d4970068487ec64454
Alessio Treglia <alessio.treglia@smartodds.co.uk>
2016-02-08 08:59:24 +0000

    Add executable example

---
+++ example_quote_unsafe_args.go
@@ -0,0 +1,16 @@
+package shellescape
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ExampleUnsageArgs(filename string) {
+	filename := "myfile; rm -rf /"
+	prog := "/bin/ls"
+	unescaped_command := strings.Join([]string{prog, filename}, " ")
+	escaped_command := strings.Join([]string{prog, Quote(filename)}, " ")
+
+	fmt.Printf("Unescaped command: %s\n", unescaped_command)
+	fmt.Printf("Escaped command: %s\n", escaped_command)
+}
