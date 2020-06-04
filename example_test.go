ba571026b6fed869b20f70942b99a79e87fbdf53
Alessio Treglia <alessio@tendermint.com>
2020-06-04 20:36:54 +0200

    tiny addition to code examples

--- example_test.go
+++ example_test.go
@@ -9,13 +9,17 @@ import (
 
 func ExampleQuote() {
 	filename := "myfile; rm -rf /"
-	prog := "/bin/ls"
+	prog := "/bin/ls -lh"
 	unescapedCommand := strings.Join([]string{prog, filename}, " ")
 	escapedCommand := strings.Join([]string{prog, shellescape.Quote(filename)}, " ")
 
 	fmt.Println(unescapedCommand)
 	fmt.Println(escapedCommand)
+	fmt.Println(strings.Split(unescapedCommand," "))
+	fmt.Println(strings.Split(escapedCommand," "))
 	// Output:
-	// /bin/ls myfile; rm -rf /
-	// /bin/ls 'myfile; rm -rf /'
+	// /bin/ls -lh myfile; rm -rf /
+	// /bin/ls -lh 'myfile; rm -rf /'
+	// [/bin/ls -lh myfile; rm -rf /]
+	// [/bin/ls -lh 'myfile; rm -rf /']
 }
