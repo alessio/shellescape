d670b35a2155e58f3798d6cc18ec19a394381db9
Alessio Treglia <alessio@tendermint.com>
2020-11-07 22:55:27 +0000

    add example

--- example_test.go
+++ example_test.go
@@ -10,16 +10,47 @@ import (
 func ExampleQuote() {
 	filename := "myfile; rm -rf /"
 	prog := "/bin/ls -lh"
-	unescapedCommand := strings.Join([]string{prog, filename}, " ")
-	escapedCommand := strings.Join([]string{prog, shellescape.Quote(filename)}, " ")
+	unsafe := strings.Join([]string{prog, filename}, " ")
+	safe := strings.Join([]string{prog, shellescape.Quote(filename)}, " ")
 
-	fmt.Println(unescapedCommand)
-	fmt.Println(escapedCommand)
-	fmt.Println(strings.Split(unescapedCommand," "))
-	fmt.Println(strings.Split(escapedCommand," "))
+	fmt.Println("unsafe:", unsafe)
+	fmt.Println("safe:", safe)
+
+	for i, part := range strings.Split(unsafe, " ") {
+		fmt.Printf("unsafe[%d] = %s\n", i, part)
+	}
+
+	for i, part := range strings.Split(safe, " ") {
+		fmt.Printf("safe[%d] = %s\n", i, part)
+	}
 	// Output:
-	// /bin/ls -lh myfile; rm -rf /
-	// /bin/ls -lh 'myfile; rm -rf /'
-	// [/bin/ls -lh myfile; rm -rf /]
-	// [/bin/ls -lh 'myfile; rm -rf /']
+	// unsafe: /bin/ls -lh myfile; rm -rf /
+	// safe: /bin/ls -lh 'myfile; rm -rf /'
+	// unsafe[0] = /bin/ls
+	// unsafe[1] = -lh
+	// unsafe[2] = myfile;
+	// unsafe[3] = rm
+	// unsafe[4] = -rf
+	// unsafe[5] = /
+	// safe[0] = /bin/ls
+	// safe[1] = -lh
+	// safe[2] = 'myfile;
+	// safe[3] = rm
+	// safe[4] = -rf
+	// safe[5] = /'
+}
+
+func ExampleQuoteCommand() {
+	filename := "filename with space"
+	prog := "/usr/bin/ls"
+	args := "-lh"
+
+	unsafe := strings.Join([]string{prog, args, filename}, " ")
+	safe := strings.Join([]string{prog, shellescape.QuoteCommand([]string{args, filename})}, " ")
+
+	fmt.Println("unsafe:", unsafe)
+	fmt.Println("safe:", safe)
+	// Output:
+	// unsafe: /usr/bin/ls -lh filename with space
+	// safe: /usr/bin/ls -lh 'filename with space'
 }
