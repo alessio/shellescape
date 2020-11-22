5aa11a0374334b526a6b41a413a86de01c0d95bc
Alessio Treglia <alessio@tendermint.com>
2020-11-22 17:13:04 +0000

    escargs: add -D flag
    The -D flag emulates xargs's default behaviour of ignoring
    blank lines on standard input.

--- cmd/escargs/escargs.go
+++ cmd/escargs/escargs.go
@@ -5,23 +5,35 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/alessio/shellescape"
 )
 
+var (
+	flagDiscardBlankLines = flag.Bool("D", false, "ignore blank lines on the standard input.")
+)
+
 func main() {
+	flag.Parse()
+
 	firstScan := true
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
+		line := scanner.Text()
+		if *flagDiscardBlankLines && len(line) == 0 {
+			continue
+		}
+
 		if firstScan {
 			firstScan = false
 		} else {
 			fmt.Printf(" ")
 		}
 
-		fmt.Printf("%s", shellescape.Quote(scanner.Text()))
+		fmt.Printf("%s", shellescape.Quote(line))
 	}
 }
